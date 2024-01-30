5fc8e437d18d79beda3a25a4870bf42e20098519
igor99995 <156124034+igor99995@users.noreply.github.com>
2024-01-30 17:04:41 -0300

    Add small changes for aesthetic purposes
    Changed the line breaks for more spacing on the console log

--- javascript.js
+++ javascript.js
@@ -52,17 +52,17 @@ function playGame() {
         let gameResult = gameRound(userChoice, computerChoice)
         if (gameResult.includes("You won")) {
             userScore++
-            console.log(gameResult + `\nYour score: ${userScore}\nComputer score: ${computerScore}`)
+            console.log(gameResult + `\n\nYour score: ${userScore}\nComputer score: ${computerScore}`)
         }
         else if (gameResult.includes("You lost")) {
             computerScore++
-            console.log(gameResult + `\nYour score: ${userScore}\nComputer score: ${computerScore}`)
+            console.log(gameResult + `\n\nYour score: ${userScore}\nComputer score: ${computerScore}`)
         }
         else {
-            console.log(gameResult + `\nYour score: ${userScore}\nComputer score: ${computerScore}`)
+            console.log(gameResult + `\n\nYour score: ${userScore}\nComputer score: ${computerScore}`)
         }
 
-        if(userScore === 5) {console.log (`Congratulations!!!\n\nYou won the rock,paper and scissors with a score of: ${userScore}`)};
-        if(computerScore === 5) {console.log(`You stupid mf!!!\n\nJust lost to a machine with the poor score of: ${userScore}`)};
+        if(userScore === 5) {console.log (`\nCongratulations!!!\n\nYou won the rock,paper and scissors with a score of ${userScore}`)};
+        if(computerScore === 5) {console.log(`\nYou stupid mf!!!\n\nJust lost to a machine with the poor score of ${userScore}`)};
     }
 }   
