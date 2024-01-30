f19015c452076a8b81e3fab8e99057a09fd388d0
igor99995 <156124034+igor99995@users.noreply.github.com>
2024-01-30 12:13:49 -0300

    Add function that plays a single round of the game
    The function returns the game result depending on the user/computer
    input, it also asks the user to input a valid value (rock,paper or
    scissors) if they didn't

--- javascript.js
+++ javascript.js
@@ -3,3 +3,42 @@ function getComputerChoice() {
     let random = Math.floor(Math.random() * choice.length)
     return choice[random]
 }
+
+function gameRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    let result
+    const validChoices = ["rock", "paper", "scissors"]
+    if (validChoices.includes(playerSelection) && playerSelection === computerSelection) {
+        result = "It's a tie!"
+    }
+    else if (!(validChoices.includes(playerSelection))) {
+        result = "Please insert a valid value"
+    }
+    else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            result = "You lost! Paper beats rock"
+        }
+        else if (computerSelection === "scissors") {
+            result = "You won! Rock beats scissors"
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            result = "You won! Paper beats rock"
+        }
+        else if (computerSelection === "scissors") {
+            result = "You lost! Scissors beats paper"
+        }
+    
+    }
+    else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            result = "You lost! Rock beats scissors"
+        }
+        else if (computerSelection === "paper") {
+            result = "You won! Scissors beats paper"
+        }
+    }
+    return result
+}
