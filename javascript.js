9ff8e8ad4425391793a6f66bdc26f87c4d02d0c8
igor99995 <156124034+igor99995@users.noreply.github.com>
2024-01-30 11:17:39 -0300

    Remove console.log used for debugging

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 function getComputerChoice() {
     let choice = ["Rock", "Paper", "Scissors"]
     let random = Math.floor(Math.random() * choice.length)
-    console.log(random)
     return choice[random]
 }
